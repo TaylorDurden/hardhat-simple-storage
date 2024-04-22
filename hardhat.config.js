e9594ab703f55511ef12e9d5c36dcc924a6377d3
taylor <258266864@qq.com>
2024-04-22 09:03:46 +0800

    etherscan api key

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -14,4 +15,7 @@ module.exports = {
     },
   },
   solidity: "0.8.24",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
